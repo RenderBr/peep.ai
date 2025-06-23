81ce2c1db813aacff543b9eab387d9e141ea8fff
Julian Seitz <julian.seitz@student.sl.on.ca>
2025-06-23 18:06:58 -0400

    Added Pinia + color mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  modules: ["shadcn-nuxt", "@nuxtjs/supabase"],
+  modules: [
+    "shadcn-nuxt",
+    "@nuxtjs/supabase",
+    "@pinia/nuxt",
+    "@nuxtjs/color-mode",
+  ],
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   css: ["~/assets/css/tailwind.css"],
@@ -11,5 +16,16 @@ export default defineNuxtConfig({
   shadcn: {
     prefix: "",
     componentDir: "./components/ui",
+  },
+  colorMode: {
+    preference: "system",
+    fallback: "light",
+    hid: "nuxt-color-mode-script",
+    globalName: "__NUXT_COLOR_MODE__",
+    componentName: "ColorScheme",
+    classPrefix: "",
+    classSuffix: "",
+    storage: "localStorage"
+    storageKey: "nuxt-color-mode",
   },
 });
